5ebf08d7e32b57e23f5a95bf4b35abe167607139
NEDNAR <drakozavrov20@gmail.com>
2022-01-20 19:59:05 +0300

    Task 1 - PlayFab UI output

--- Assets/Code/PlayFabLogin.cs
+++ Assets/Code/PlayFabLogin.cs
@@ -1,11 +1,28 @@
 using PlayFab;
 using PlayFab.ClientModels;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public sealed class PlayFabLogin : MonoBehaviour
 {
+    [SerializeField] private TMP_Text _text;
+    [SerializeField] private Button _button;
+    [SerializeField] private Color _successColor;
+    [SerializeField] private Color _failureColor;
+    
     private void Start()
+    {
+        _button.onClick.AddListener(TryLogin);
+    }
+
+    private void OnDestroy()
+    {
+        _button.onClick.RemoveListener(TryLogin);
+    }
+
+    private void TryLogin()
     {
         if (string.IsNullOrEmpty(PlayFabSettings.staticSettings.TitleId))
         {
@@ -19,11 +36,17 @@ public sealed class PlayFabLogin : MonoBehaviour
 
     private void OnLoginSuccess(LoginResult result)
     {
-        Debug.Log("PlayFab Success!");
+        var message = "PlayFab Success";
+        _text.text = message;
+        _text.color = _successColor;
+        Debug.Log(message);
     }
 
     private void OnLoginFail(PlayFabError error)
     {
-        Debug.LogError($"PlayFab Fail!: {error}");
+        var message = "PlayFab Fail!";
+        _text.text = message;
+        _text.color = _failureColor;
+        Debug.LogError($"{message}: {error}");
     }
 }
