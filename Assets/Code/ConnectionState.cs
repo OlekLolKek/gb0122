76ec09710696cb34e782181e5a269aee53ce4fd6
NEDNAR <drakozavrov20@gmail.com>
2022-01-20 21:16:13 +0300

    Task 3 - Photon button with 3 states

---
+++ Assets/Code/ConnectionState.cs
@@ -0,0 +1,7 @@
+public enum ConnectionState
+{
+    None = 0,
+    Disconnected = 1,
+    Connecting = 2,
+    Connected = 3
+}
--- Assets/Code/PhotonLogin.cs
+++ Assets/Code/PhotonLogin.cs
@@ -1,4 +1,5 @@
 using Photon.Pun;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,15 +7,24 @@ using UnityEngine.UI;
 public sealed class PhotonLogin : MonoBehaviourPunCallbacks
 {
     [SerializeField] private Button _connectButton;
-    [SerializeField] private Button _disconnectButton;
-    
+
+    private TMP_Text _buttonText;
+    private Image _buttonImage;
+
     private void Awake()
     {
         PhotonNetwork.AutomaticallySyncScene = true;
         _connectButton.onClick.AddListener(Connect);
-        _disconnectButton.onClick.AddListener(Disconnect);
+        _buttonText = _connectButton.GetComponentInChildren<TMP_Text>();
+        _buttonImage = _connectButton.GetComponentInChildren<Image>();
+        SwitchButton(ConnectionState.Disconnected);
     }
-    
+
+    private void OnDestroy()
+    {
+        _connectButton.onClick.RemoveAllListeners();
+    }
+
     public void Connect()
     {
         if (PhotonNetwork.IsConnected)
@@ -25,6 +35,8 @@ public sealed class PhotonLogin : MonoBehaviourPunCallbacks
         {
             PhotonNetwork.ConnectUsingSettings();
         }
+
+        SwitchButton(ConnectionState.Connecting);
     }
 
     private void Disconnect()
@@ -39,17 +51,41 @@ public sealed class PhotonLogin : MonoBehaviourPunCallbacks
             PhotonNetwork.Disconnect();
             Debug.Log("Photon disconnected.");
         }
+
+        SwitchButton(ConnectionState.Disconnected);
+    }
+
+    private void SwitchButton(ConnectionState state)
+    {
+        switch (state)
+        {
+            case ConnectionState.Disconnected:
+                _connectButton.onClick.RemoveAllListeners();
+                _connectButton.onClick.AddListener(Connect);
+                _connectButton.interactable = true;
+                _buttonImage.color = Color.cyan;
+                _buttonText.text = "Connect";
+                break;
+            case ConnectionState.Connected:
+                _connectButton.onClick.RemoveAllListeners();
+                _connectButton.onClick.AddListener(Disconnect);
+                _connectButton.interactable = true;
+                _buttonImage.color = Color.red;
+                _buttonText.text = "Disconnect";
+                break;
+            case ConnectionState.Connecting:
+                _connectButton.onClick.RemoveAllListeners();
+                _connectButton.interactable = false;
+                _buttonImage.color = Color.yellow;
+                _buttonText.text = "Connecting...";
+                break;
+        }
     }
 
     public override void OnConnectedToMaster()
     {
         base.OnConnectedToMaster();
         Debug.Log("Photon Connected to master.");
-    }
-
-    public override void OnJoinedRoom()
-    {
-        base.OnJoinedRoom();
-        Debug.Log("Photon joined a room.");
+        SwitchButton(ConnectionState.Connected);
     }
 }
