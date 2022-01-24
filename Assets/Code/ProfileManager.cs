e035e680a06def7b5cdd3b13a599105702a1f565
NEDNAR <drakozavrov20@gmail.com>
2022-01-24 18:54:27 +0300

    Task 2 - more user info on the MainProfile scene

--- Assets/Code/ProfileManager.cs
+++ Assets/Code/ProfileManager.cs
@@ -10,12 +10,20 @@ public sealed class ProfileManager : MonoBehaviour
     
     private void Start()
     {
+        _idText.text = "Getting user info...";
+        _idText.color = Color.yellow;
         PlayFabClientAPI.GetAccountInfo(new GetAccountInfoRequest(), success =>
         {
-            _idText.text = $"Welcome back, Player {success.AccountInfo.PlayFabId}";
+            _idText.text = $"Welcome back, {success.AccountInfo.Username}\n{GetPlayerInfo(success.AccountInfo)}";
+            _idText.color = Color.white;
         }, error =>
         {
             _idText.text = $"Something went wrong: {error}";
         });
+    }
+
+    private string GetPlayerInfo(UserAccountInfo info)
+    {
+        return $"Account created on {info.Created}\nCustom id: {info.CustomIdInfo.CustomId}\nPlayFab id: {info.PlayFabId}";
     }
 }
