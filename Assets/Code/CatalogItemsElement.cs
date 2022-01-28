e44eb54791d64f42afe2c3d180beb88bdb23bf5a
NEDNAR <drakozavrov20@gmail.com>
2022-01-29 02:29:58 +0300

    Task 4 - added fake purchasing buttons and logs

--- Assets/Code/CatalogItemsElement.cs
+++ Assets/Code/CatalogItemsElement.cs
@@ -2,19 +2,35 @@ using System;
 using PlayFab.ClientModels;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public sealed class CatalogItemsElement : MonoBehaviour
 {
     [SerializeField] private TMP_Text _displayNameText;
     [SerializeField] private TMP_Text _priceText;
+    [SerializeField] private Button _buyButton;
 
+    private CatalogItem _item;
+
+    public event Action<CatalogItem> OnButtonPressed = delegate { };
+    
     public void SetItem(CatalogItem item)
     {
-        _displayNameText.text = item.DisplayName;
+        _item = item;
+        _displayNameText.text = _item.DisplayName;
         _priceText.text
-            = item.VirtualCurrencyPrices.ContainsKey("SB")
-            ? item.VirtualCurrencyPrices["SB"].ToString()
+            = _item.VirtualCurrencyPrices.ContainsKey(Constants.SBEU_CURRENCY_ID)
+            ? _item.VirtualCurrencyPrices[Constants.SBEU_CURRENCY_ID].ToString()
             : string.Empty;
+        
+        _buyButton.onClick.AddListener(ButtonPressed);
+        Debug.Log("Set button");
+    }
+
+    private void ButtonPressed()
+    {
+        Debug.Log("Button pressed");
+        OnButtonPressed.Invoke(_item);
     }
 }
--- Assets/Code/CatalogManager.cs
+++ Assets/Code/CatalogManager.cs
@@ -1,36 +1,88 @@
+using System;
 using System.Collections.Generic;
 using PlayFab;
 using PlayFab.ClientModels;
+using TMPro;
 using UnityEngine;
 
 
 public sealed class CatalogManager : MonoBehaviour
 {
-    [SerializeField] private CatalogItemsElement _element;
+    [SerializeField] private CatalogItemsElement _elementPrefab;
     [SerializeField] private Transform _elementsRoot;
+    [SerializeField] private TMP_Text _currencyText;
+
+    private int _currency;
     
     private readonly Dictionary<string, CatalogItem> _catalog = new Dictionary<string, CatalogItem>();
+    private readonly List<CatalogItemsElement> _elements = new List<CatalogItemsElement>();
 
     private void Start()
     {
-        PlayFabClientAPI.GetCatalogItems(new GetCatalogItemsRequest(), result =>
+        PlayFabClientAPI.GetCatalogItems(new GetCatalogItemsRequest(), result => { HandleCatalog(result.Catalog); },
+            error => { Debug.LogError($"Get catalog items failed: {error}"); });
+
+        _currencyText.text = "Loading currency...";
+        GetCurrency(result =>
         {
-            HandleCatalog(result.Catalog);
-        }, error =>
-        {
-            Debug.LogError($"Get catalog items failed: {error}");
+            _currencyText.text = $"Currency: {_currency}";
         });
     }
-    
+
     private void HandleCatalog(List<CatalogItem> catalog)
     {
         foreach (var item in catalog)
         {
             _catalog.Add(item.ItemId, item);
             Debug.Log($"item with ID {item.ItemId} was added to dictionary");
-            var element = Instantiate(_element, _elementsRoot);
-            element.gameObject.SetActive(true);
-            element.SetItem(item);
+
+            if (item.VirtualCurrencyPrices.ContainsKey(Constants.SBEU_CURRENCY_ID))
+            {
+                var element = Instantiate(_elementPrefab, _elementsRoot);
+                element.gameObject.SetActive(true);
+                element.SetItem(item);
+                element.OnButtonPressed += ElementBuyButtonPressed;
+                _elements.Add(element);
+            }
+        }
+    }
+
+    private void ElementBuyButtonPressed(CatalogItem item)
+    {
+        GetCurrency(result =>
+        {
+            if (_currency >= item.VirtualCurrencyPrices["SB"])
+            {
+                Debug.Log($"<color=green>Successfully purchased {item.DisplayName}! currency: {_currency}</color>");
+            }
+            else
+            {
+                Debug.Log($"<color=red>Not enough currency to purchase {item.DisplayName} :( currency: {_currency}</color>");
+            }
+        }) ;
+    }
+
+    private void GetCurrency(Action<GetUserInventoryResult> successCallback)
+    {
+        PlayFabClientAPI.GetUserInventory(new GetUserInventoryRequest(), result =>
+        {
+            if (result.VirtualCurrency.ContainsKey(Constants.SBEU_CURRENCY_ID))
+            {
+                _currency = result.VirtualCurrency[Constants.SBEU_CURRENCY_ID];
+                successCallback?.Invoke(result);
+            }
+        }, error =>
+        {
+            Debug.LogError($"Get user inventory request failed: {error}");
+            _currency = int.MinValue;
+        });
+    }
+
+    private void OnDestroy()
+    {
+        foreach (var element in _elements)
+        {
+            element.OnButtonPressed -= ElementBuyButtonPressed;
         }
     }
 }
