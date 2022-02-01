e1121c1ee1e21528b5d0dfa2235e821266e2e0cd
NEDNAR <drakozavrov20@gmail.com>
2022-02-02 02:18:29 +0300

    Added change username feature

--- Assets/Code/ProfileManager.cs
+++ Assets/Code/ProfileManager.cs
@@ -8,24 +8,58 @@ using UnityEngine.UI;
 
 public sealed class ProfileManager : MonoBehaviour
 {
+    [SerializeField] private ChangeUsernamePanelView _changeUsernamePanelView;
+    [SerializeField] private TMP_Text _usernameText;
     [SerializeField] private TMP_Text _idText;
+    [SerializeField] private Button _changeUsernameButton;
     [SerializeField] private Button _backButton;
-    
+
     private void Start()
     {
-        _idText.text = "Getting user info...";
-        _idText.color = Color.yellow;
-        PlayFabClientAPI.GetAccountInfo(new GetAccountInfoRequest(), success =>
-        {
-            _idText.text = $"{success.AccountInfo.Username}\nID: {success.AccountInfo.PlayFabId}";
-            _idText.color = Color.white;
-        }, error =>
-        {
-            _idText.text = $"Something went wrong: {error}";
-            _idText.color = Color.red;
-        });
-        
+        _usernameText.text = "Getting user info...";
+        _usernameText.color = Color.yellow;
+        _idText.text = string.Empty;
+        UpdateUserInfo();
+
         _backButton.onClick.AddListener(GoToBootstrap);
+        _changeUsernameButton.onClick.AddListener(OpenChangeUsernamePanel);
+        _changeUsernamePanelView.OnConfirmButtonClicked += ChangeUsername;
+    }
+
+    private void UpdateUserInfo()
+    {
+        PlayFabClientAPI.GetPlayerProfile(new GetPlayerProfileRequest(), result =>
+        {
+            SetUsernameText(result.PlayerProfile.DisplayName);
+            SetIdText(result.PlayerProfile.PlayerId);
+        }, Debug.LogError);
+    }
+
+    private void SetUsernameText(string username)
+    {
+        _usernameText.text = $"{username}";
+        _usernameText.color = Color.white;
+    }
+    
+    private void SetIdText(string id)
+    {
+        _idText.text = $"{id}";
+        _idText.color = Color.white;
+    }
+
+    private void OpenChangeUsernamePanel()
+    {
+        _changeUsernamePanelView.gameObject.SetActive(true);
+    }
+
+    private void ChangeUsername(string newUsername)
+    {
+        _changeUsernamePanelView.gameObject.SetActive(false);
+        PlayFabClientAPI.UpdateUserTitleDisplayName(new UpdateUserTitleDisplayNameRequest { DisplayName = newUsername },
+            result =>
+            {
+                SetUsernameText(result.DisplayName);
+            }, Debug.LogError);
     }
 
     private void GoToBootstrap()
@@ -36,5 +70,7 @@ public sealed class ProfileManager : MonoBehaviour
     private void OnDestroy()
     {
         _backButton.onClick.RemoveListener(GoToBootstrap);
+        _changeUsernameButton.onClick.RemoveListener(OpenChangeUsernamePanel);
+        _changeUsernamePanelView.OnConfirmButtonClicked -= ChangeUsername;
     }
 }
---
+++ Assets/Code/Views/ChangeUsernamePanelView.cs
@@ -0,0 +1,36 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public sealed class ChangeUsernamePanelView : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField _usernameInputField;
+    [SerializeField] private Button _confirmButton;
+    [SerializeField] private Button _backButton;
+    
+    public event Action<string> OnConfirmButtonClicked = delegate {  };
+
+    private void Start()
+    {
+        _confirmButton.onClick.AddListener(ConfirmButtonClicked);
+        _backButton.onClick.AddListener(BackButtonClicked);
+    }
+
+    private void OnDestroy()
+    {
+        _confirmButton.onClick.RemoveListener(ConfirmButtonClicked);
+        _backButton.onClick.RemoveListener(BackButtonClicked);
+    }
+
+    private void ConfirmButtonClicked()
+    {
+        OnConfirmButtonClicked.Invoke(_usernameInputField.text);
+    }
+
+    private void BackButtonClicked()
+    {
+        gameObject.SetActive(false);
+    }
+}
