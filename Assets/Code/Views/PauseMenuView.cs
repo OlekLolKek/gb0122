b3823bfcdd8afc8873db28de0b3084963593c06f
NEDNAR <drakozavrov20@gmail.com>
2022-03-22 20:06:42 +0300

    Added a sound settings menu

--- Assets/Code/Views/PauseMenuView.cs
+++ Assets/Code/Views/PauseMenuView.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 
@@ -6,24 +7,37 @@ public sealed class PauseMenuView : MonoBehaviour
 {
     [Header("Panels")]
     [SerializeField] private ChangeUsernamePanelView _changeUsernamePanel;
-    
-    [Header("Buttons")]
+
+    [Header("Controls")]
+    [SerializeField] private Slider _musicVolumeSlider;
+    [SerializeField] private Slider _soundVolumeSlider;
     [SerializeField] private Button _closeMenuButton;
     [SerializeField] private Button _changeUsernameButton;
     [SerializeField] private Button _quitButton;
 
+    [Header("Audio Mixer Groups")]
+    [SerializeField] private AudioMixer _audioMixer;
+    [SerializeField] private string _musicVolumeKey;
+    [SerializeField] private string _soundVolumeKey;
+
     private void Start()
     {
+        _musicVolumeSlider.onValueChanged.AddListener(MusicVolumeChanged);
+        _soundVolumeSlider.onValueChanged.AddListener(SoundVolumeChanged);
         _closeMenuButton.onClick.AddListener(CloseMenuButtonClicked);
         _changeUsernameButton.onClick.AddListener(ChangeUsernameButtonClicked);
         _quitButton.onClick.AddListener(QuitButtonClicked);
 
         _changeUsernamePanel.OnBackButtonClicked += CloseChangeUsernamePanel;
         _changeUsernamePanel.OnConfirmButtonClicked += ConfirmChangeUsernameButtonClicked;
+
+        SetSliderValues();
     }
 
     private void OnDestroy()
     {
+        _musicVolumeSlider.onValueChanged.RemoveListener(MusicVolumeChanged);
+        _soundVolumeSlider.onValueChanged.RemoveListener(SoundVolumeChanged);
         _closeMenuButton.onClick.RemoveListener(CloseMenuButtonClicked);
         _changeUsernameButton.onClick.RemoveListener(ChangeUsernameButtonClicked);
         _quitButton.onClick.RemoveListener(QuitButtonClicked);
@@ -36,6 +50,25 @@ public sealed class PauseMenuView : MonoBehaviour
         _quitButton.gameObject.SetActive(true);
         
         gameObject.SetActive(true);
+    }
+
+    private void SetSliderValues()
+    {
+        _audioMixer.GetFloat(_musicVolumeKey, out var musicVolume);
+        _musicVolumeSlider.value = GetSliderValue(musicVolume);
+        
+        _audioMixer.GetFloat(_soundVolumeKey, out var soundVolume);
+        _soundVolumeSlider.value = GetSliderValue(soundVolume);
+    }
+
+    private void MusicVolumeChanged(float sliderValue)
+    {
+        _audioMixer.SetFloat(_musicVolumeKey, GetDecibels(sliderValue));
+    }
+
+    private void SoundVolumeChanged(float sliderValue)
+    {
+        _audioMixer.SetFloat(_soundVolumeKey, GetDecibels(sliderValue));
     }
 
     private void ChangeUsernameButtonClicked()
@@ -66,6 +99,17 @@ public sealed class PauseMenuView : MonoBehaviour
         _quitButton.gameObject.SetActive(false);
         
         gameObject.SetActive(false);
+    }
+
+    private float GetDecibels(float sliderValue)
+    {
+        return Mathf.Log10(sliderValue) * 20;
+    }
+
+    private float GetSliderValue(float decibels)
+    {
+        var log10 = decibels / 20;
+        return Mathf.Pow(10, log10);
     }
 
     private void QuitButtonClicked()
